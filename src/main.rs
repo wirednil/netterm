a4f696e472c619e2dd6d0fb7ff42adf7bb2a924e
wirednil <diogenescristaldo@gmail.com>
2025-01-05 17:53:39 -0300

    **Descripción:** Se han añadido etiquetas `vertical` y `horizontal` para la estructura `Form` en el archivo `form.rs`.
    **Detalles:**
    - Se ha actualizado la estructura `Container` para incluir las etiquetas
    `vertical` y `horizontal`, permitiendo la anidación de contenedores de
    forma vertical y horizontal.
    - Se ha modificado la estructura `Form` para incluir una raíz vertical
    (`root_vertical`) que puede contener múltiples contenedores.

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,12 @@ use crate::form::Form;
 
 fn main() {
     info!("Inicio del programa.");
-    let fm_result: Result<Form, std::io::Error> = Form::new("clie.form");
+    let fm_result: Result<Form, std::io::Error> = Form::new("login.form");
     match fm_result {
         Ok(mut fm) => {
             println!("Form loaded successfully.");
             fm.make_form();
+
         },
         Err(e) => {
             eprintln!("Error loading form: {}", e);
